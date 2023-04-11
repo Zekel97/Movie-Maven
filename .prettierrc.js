b555e9f4e968ba7a4583d0e634ebbdb4218bceaf
Zekel97 <szandreas97@gmail.com>
2023-04-11 18:44:46 +0200

    Basic skeleton and dependencies implementation

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  printWidth: 125,
+  trailingComma: 'all',
+  singleQuote: true,
+  plugins: [require('prettier-plugin-tailwindcss')],
+  tailwindConfig: 'tailwind.config.js',
+};
