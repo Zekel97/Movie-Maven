815c88d7aafa40faaa5d4916c05714b97ee2d129
Zekel97 <szandreas97@gmail.com>
2023-04-11 22:14:10 +0200

    Implementing movie data structure with mock data

--- components/MovieBody/movieBody.tsx
+++ components/MovieBody/movieBody.tsx
@@ -1,23 +1,64 @@
 import styles from './movieBody.module.scss';
 
+import { useMovieDataContext } from '@/scripts/MovieDataContext';
+
 export default function MovieBody() {
   // TODO: temp variable to test conditional rendering
-  const foundMovie = true;
+  const { foundMovie } = useMovieDataContext();
 
   return (
     <div className={styles.body}>
       {foundMovie ? (
         <div className={styles.movie}>
           <div className={styles.movieHeader}>
-            <h2>Movie Title</h2>
-            <p>Movie Description</p>
+            <h2>{foundMovie.title}</h2>
+            <p>{foundMovie.year}</p>
           </div>
-          <div className={styles.moviePoster}></div>
+
+          <div className={styles.moviePoster}>
+            <img src={foundMovie.poster} alt={foundMovie.title} />
+          </div>
+
+          <div className={styles.movieInfo}>
+            <section>
+              <h3>Plot:</h3>
+              <p>{foundMovie.plot}</p>
+            </section>
+            <section>
+              <h3>Director:</h3>
+              <p>{foundMovie.director}</p>
+            </section>
+            <section>
+              <h3>Writer:</h3>
+              <p>{foundMovie.writer}</p>
+            </section>
+            <section>
+              <h3>Actors:</h3>
+              <p>{foundMovie.actors}</p>
+            </section>
+            <section>
+              <h3>Genre:</h3>
+              <p>{foundMovie.genre}</p>
+            </section>
+            <section>
+              <h3>Runtime:</h3>
+              <p>{foundMovie.runtime}</p>
+            </section>
+          </div>
+
           <div className={styles.movieRatings}>
             <h3>Ratings:</h3>
             <ul>
-              <li>IMDB: 8.5</li>
-              <li>Rotten Tomatoes: 90%</li>
+              <li>
+                <div className={styles.ratingSource}>Metascore</div>
+                <div className={styles.ratingValue}>{foundMovie.metascore}</div>
+              </li>
+              {foundMovie.ratings.map((rating) => (
+                <li key={rating.source}>
+                  <div className={styles.ratingSource}>{rating.source}</div>
+                  <div className={styles.ratingValue}>{rating.value}</div>
+                </li>
+              ))}
             </ul>
           </div>
         </div>
--- scripts/MovieDataContext.tsx
+++ scripts/MovieDataContext.tsx
@@ -4,11 +4,33 @@ interface Props {
   children: ReactNode;
 }
 
+interface MovieResponseInterface {
+  title: string;
+  year: string;
+  rated: string;
+  released: string;
+  runtime: string;
+  genre: string;
+  director: string;
+  writer: string;
+  actors: string;
+  plot: string;
+  poster: string;
+  ratings: {
+    source: string;
+    value: string;
+  }[];
+  metascore: string;
+  imdbID: string;
+  type: string;
+}
+
 interface MovieDataInterface {
   search: {
     movieName: string;
     setMovieName: (movieName: string) => void;
   };
+  foundMovie?: MovieResponseInterface;
 }
 
 const MovieDataContext = createContext({} as MovieDataInterface);
@@ -20,11 +42,36 @@ export function useMovieDataContext() {
 export function MovieDataProvider({ children }: Props) {
   const [searchedMovieName, setSearchedMovieName] = useState('');
 
+  // TODO: dummy data
+  const foundMovie: MovieResponseInterface = {
+    title: 'The Matrix',
+    year: '1999',
+    rated: 'R',
+    released: '31 Mar 1999',
+    runtime: '136 min',
+    genre: 'Action, Sci-Fi',
+    director: 'Lana Wachowski, Lilly Wachowski',
+    writer: 'Lilly Wachowski, Lana Wachowski',
+    actors: 'Keanu Reeves, Laurence Fishburne, Carrie-Anne Moss, Hugo Weaving',
+    plot: 'A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.',
+    poster: 'https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg',
+    ratings: [
+      {
+        source: 'Internet Movie Database',
+        value: '8.7/10',
+      },
+    ],
+    metascore: '73',
+    imdbID: 'tt0133093',
+    type: 'movie',
+  }
+
   const value: MovieDataInterface = {
     search: {
       movieName: searchedMovieName,
       setMovieName: (movieName: string) => setSearchedMovieName(movieName),
     },
+    foundMovie,
   };
 
   return <MovieDataContext.Provider value={value}>{children}</MovieDataContext.Provider>;
